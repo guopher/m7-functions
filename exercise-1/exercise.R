97b8c794db508762a678f62531e4c50fe60897d4
JJ Guo <jjguo123@gmail.com>
2017-06-03 16:09:21 -0700

    Finished assignment

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,13 +1,24 @@
 # Exercise 1: writing and executing functions
 
 # Write a function `AddThree` that adds 3 to an input value
+AddThree <- function(a) {
+  a = a + 3
+  print(a)
+}
 
+ten = 7
+AddThree(ten)
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
 
-
 # Write a function `FeetToMeters` that converts from feet to meters
 
+FeetToMeters <- function(feet) {
+  meters = feet * 0.3048
+  print(meters)
+}
+height.in.feet = 5.6
+FeetToMeters(height.in.feet)
 
 # Create a variable `height.in.feet` that is your height in feet
 
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,6 +3,15 @@
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
+CompareLength <- function(v1, v2) {
+  difference = length(v1) - length(v2)
+  difference <- as.character(difference)
+  line = paste("The difference in lengths is ", difference)
+  print(line)
+}
+
+CompareLength(c(3, 4, 5), c(3, 4))
+
 
 # Pass two vectors of different length to your `CompareLength` function
 
@@ -11,9 +20,26 @@
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
 
+DescribeDifference <- function(v1, v2) {
+  difference = length(v1) - length(v2)
+  line = ""
+  if (difference > 0) {
+    difference <- as.character(difference)
+    line = paste("Your first vector is longer by ", difference, "elements")
+  } else if (difference < 0) {
+    difference <- as.character(difference * -1)
+    line = paste("Your second vector is longer by ", difference, "elements")
+  } else {
+    line = "The vectors have the same length"
+  }
+  print(line)
+}
+
+DescribeDifference(c(3, 4), c(3, 4, 5, 6))
+DescribeDifference(c(3, 4, 5, 6), c(3, 4))
+DescribeDifference(c(1), c(2))
 
 # Pass two vectors to your `DescribeDifference` function
-
 
 ### Bonus ###
 
